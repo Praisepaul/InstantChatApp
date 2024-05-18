eb4586601817e47dcbeafb016ec16189e527e3a3
Praise Paul <praise20024christ@gmail.com>
2024-05-18 22:43:36 +0530

    Routing Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,17 @@ import Home from './pages/home/home'
 import Login from './pages/login/login'
 import SignUp from './pages/signUp/signUp'
 
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        {/* <SignUp /> */}
-        <Home />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
       </div>
-    </>
   )
 }
 
